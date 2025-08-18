c0eb9c9db614bcac5d8566ac8a2762406233b128
SmtT23 <infin@coalition.dev>
2025-08-18 12:18:13 +0100

    Initial commit: coalition bootstrap script

---
+++ termux_bootstrap.sh
@@ -0,0 +1,6 @@
+git init
+git add termux_bootstrap.sh
+git commit -m "Initial commit: coalition bootstrap script"
+git remote add origin https://github.com/SmtT23/coalition-bootstrap.git
+git branch -M main
+git push -u origin main
